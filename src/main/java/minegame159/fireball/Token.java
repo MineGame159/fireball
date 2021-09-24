616c016c986806762bb689e7c461921a86aa2695
arlomcwalter <mcwalterarlo@gmail.com>
2021-09-24 15:27:39 +0100

    Created LexerTest as to separate tests from the language (#1)

--- src/main/java/minegame159/fireball/Token.java
+++ src/main/java/minegame159/fireball/Token.java
@@ -1,13 +1,3 @@
 package minegame159.fireball;
 
-public class Token {
-    public final TokenType type;
-    public final String lexeme;
-    public final int line;
-
-    public Token(TokenType type, String lexeme, int line) {
-        this.type = type;
-        this.lexeme = lexeme;
-        this.line = line;
-    }
-}
+public record Token(TokenType type, String lexeme, int line) {}
